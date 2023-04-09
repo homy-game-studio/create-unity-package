5396f642a53d86adb65c43ecf50379c4138ba187
Matheus Costa <matheus.design12@gmail.com>
2023-04-08 23:12:27 -0300

    feat: Add Github Deploy

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@
 
 import { oraPromise } from "ora";
 import { createDirectory } from "./modules/directory";
-import { createRepo as createGitRepo } from "./modules/git";
+import { createGitHubRepo } from "./modules/github";
+import { createRepo as createGitRepo, initAndPushRepo } from "./modules/git";
 import input from "./modules/input";
 import { render as renderTemplate } from "./modules/render";
 import { createUnityProject, getUnityInstallations, openUnityProject, parseUnityVersion } from "./modules/unity";
@@ -22,12 +23,16 @@ const main = async () => {
     const unityPath = unityInstallations[inputData.unityVersion];
     const projectPath = await run(createDirectory(inputData.packageName), "Create directory");
     await run(createUnityProject(unityPath, projectPath), "Create unity project");
-    await run(createGitRepo(projectPath), "Create git repo");
+    
+    const git = await run(createGitRepo(projectPath), "Create git repo");
+    const cloneUrl = await createGitHubRepo(inputData);
+    
     const renderData = {
       ...inputData,
       unityVersion: parseUnityVersion(inputData.unityVersion)
     }
     await run(renderTemplate(renderData, projectPath), "Render template");
+    await run(initAndPushRepo(git, inputData, cloneUrl), "Deploy on Github repository");
     await run(openUnityProject(unityPath, projectPath), "Open unity project");
   } catch (e) {
     console.error(e);
